3e980a574bdc16ab97bb69a7d0b9bad38182a0cb
stelgkio <gkiostyl13@gmail.com>
2024-09-05 15:56:47 +0300

    toml

--- internal/adapter/web/view/component/project/settings/notification/settings_notification_templ.go
+++ internal/adapter/web/view/component/project/settings/notification/settings_notification_templ.go
@@ -39,6 +39,10 @@ func SettingsNotifications(project *domain.Project) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = NotifiactionTable(project.Id.String()).Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</main></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
--- internal/core/domain/notification.go
+++ internal/core/domain/notification.go
@@ -1,15 +1,23 @@
 package domain
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 // Notification represents a notification
 type Notification struct {
-	MongoBase
-	Title       string `json:"title" bson:"title,omitempty"`
-	Description string `json:"description" bson:"description,omitempty"`
-	Link        string `json:"link" bson:"link,omitempty"`
-	IsActive    bool   `json:"is_active" bson:"is_active,omitempty"`
-	IsRead      bool   `json:"is_read" bson:"is_read,omitempty"`
-	IsDeleted   bool   `json:"is_deleted" bson:"is_deleted,omitempty"`
-	UserID      string `json:"user_id" bson:"user_id,omitempty"`
-	ProjectID   string `json:"project_id" bson:"project_id,omitempty"`
-	Type        string `json:"type" bson:"type,omitempty"`
+	ID          primitive.ObjectID `bson:"_id,omitempty"`
+	Title       string             `json:"title" bson:"title,omitempty"`
+	Description string             `json:"description" bson:"description,omitempty"`
+	Link        string             `json:"link" bson:"link,omitempty"`
+	IsRead      bool               `json:"is_read" bson:"is_read,omitempty"`
+	CreatedAt   time.Time          `json:"created_at"  bson:"created_at,omitempty"`
+	UpdatedAt   time.Time          `json:"updated_at"  bson:"updated_at,omitempty"`
+	DeletedAt   time.Time          `json:"deleted_at"  bson:"deleted_at,omitempty"`
+	IsActive    bool               `json:"is_active" bson:"is_active,omitempty"`
+	UserID      string             `json:"user_id" bson:"user_id,omitempty"`
+	ProjectID   string             `json:"project_id" bson:"project_id,omitempty"`
+	Type        string             `json:"type" bson:"type,omitempty"`
 }

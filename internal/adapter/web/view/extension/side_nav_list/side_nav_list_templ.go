ebcf8cc57094869b114143c058c5c1d706164890
stelgkio <gkiostyl13@gmail.com>
2024-10-09 14:52:25 +0300

    remove courier and show only one

--- internal/adapter/web/view/extension/side_nav_list/side_nav_list_templ.go
+++ internal/adapter/web/view/extension/side_nav_list/side_nav_list_templ.go
@@ -102,7 +102,7 @@ func SideNavList(projectId, extensionId string, extensions []*domain.ProjectExte
 				}
 			}
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul></div></li>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</ul></div></li><script>\n    document.addEventListener('DOMContentLoaded', function() {\n        let courierItems = document.querySelectorAll('.extension-item[data-extension-code=\"asc-courier\"], .extension-item[data-extension-code=\"courier4u\"], .extension-item[data-extension-code=\"redcourier\"]');\n        if (courierItems.length > 0) {\n            courierItems[0].querySelector('a').textContent = 'Courier';\n            for (let i = 1; i < courierItems.length; i++) {\n                courierItems[i].remove();\n            }\n        }\n    });\n</script>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
